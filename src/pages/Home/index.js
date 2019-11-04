974cbd5d2dc76f571ae1492936442c9e4399bdd0
Alexddsilva <Alex.dds@hotmail.com>
2019-11-04 17:17:39 -0300

    style.css no home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './style.css';
 
-export default function Home(){
+export default function Home({ history }){  
 
-  return <h1>CRUD FUNCIONÁRIOS</h1>
+  return (
+    <div className="home">
+      <h1>CRUD FUNCIONÁRIOS</h1>
+      <button className="btn_cadastrar" onClick={() => history.push('/api/funcionarios')}>Cadastrar Funcionários</button>
+      <button className="btn_listar" onClick={() => history.push('/api/listagem')}>Lista de Funcionários</button>
+    </div>
+    
+  )
 }
