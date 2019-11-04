ac006be91b1c4fc431dbc6c53a43a468460a4369
Alexddsilva <Alex.dds@hotmail.com>
2019-11-04 20:31:02 -0300

    rotas

--- src/pages/Listagem/index.js
+++ src/pages/Listagem/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import api from '../../services/api';
 
 export default function Listagem(){
+  const funcionarios = api.get('/api/funcionarios', [])
+  console.log(funcionarios);
+  
 
   return (
    <>
-    
+      
    </>
   )
 }
