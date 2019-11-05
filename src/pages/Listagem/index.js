5894d262457a1aff5233d24b9fbc6bf6e79d4b79
Alexddsilva <Alex.dds@hotmail.com>
2019-11-05 09:44:34 -0300

    recebimento dos dados

--- src/pages/Listagem/index.js
+++ src/pages/Listagem/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 export default function Listagem(){
-  const funcionarios = api.get('/api/funcionarios', [])
-  console.log(funcionarios);
+  const [funcionarios, setFuncionarios] = useState([]);
+
+  useEffect(() => {
+    async function loadFuncionarios () {
+      const response = await api.get('/api/funcionarios')
+      console.log(response);
+
+      setFuncionarios(response.data);
+    }
+    loadFuncionarios();
+  }, []);
+
+  /*const funcionarios = api.get('/api/funcionarios', [])
+  console.log(funcionarios);*/
   
+
 
   return (
    <>
+    <ul className="lista-funcionarios">
+      {funcionarios.map(funcionario => (
+          <p key={funcionario.id}>{funcionario.nome}</p>
+        ))
+      }
+    </ul>
       
    </>
   )
