cd06c933f1fa66c9229325e07e05c1165fe0083c
Alexddsilva <Alex.dds@hotmail.com>
2019-11-05 14:43:03 -0300

    tabela

--- src/pages/Listagem/index.js
+++ src/pages/Listagem/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import './style.css';
 
 export default function Listagem(){
   const [funcionarios, setFuncionarios] = useState([]);
@@ -16,9 +17,19 @@ export default function Listagem(){
 
   return (
    <>
+    <th>Nome</th>
+    <th>Sobrenome</th>
+    <th>E-mail</th>
+    <th>nis</th>
+    <th>Opções</th>
     <ul className="lista-funcionarios">
       {funcionarios.map(funcionario => (
-          <li key={funcionario.id}>{funcionario.nome} {funcionario.sobrenome} {funcionario.email} {funcionario.nis}</li>
+          <li key={funcionario.id}>
+            <td>{funcionario.nome}</td> 
+            <td>{funcionario.sobrenome}</td> 
+            <td>{funcionario.email}</td> 
+            <td>{funcionario.nis}</td> 
+          </li>
         ))
       }
     </ul>
