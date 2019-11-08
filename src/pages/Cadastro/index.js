c148dad028ad037728ff1cc23d251294aef07992
Alexddsilva <Alex.dds@hotmail.com>
2019-11-08 14:04:28 -0300

    Patterns nos inputs

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import './style.css';
 
-export default function Cadastro(){
+export default function Cadastro({ history }){
   let funcionario = {
     id: 0,
     nome:'',
@@ -12,59 +11,78 @@ export default function Cadastro(){
     nis:''
   }
 
-  async function handleSubmit(event){
-    event.preventDefault();
-    await api.post('/api/funcionarios', {
+  async function handleSubmit(){
+    
+
+    const response = await api.post('/api/funcionarios', {
       "nome": funcionario.nome,
       "sobrenome": funcionario.sobrenome,
       "email": funcionario.email,
       "nis": funcionario.nis
     })
+    console.log(response.data);
+    history.push('/api/funcionarios');
   }
 
-  return ( /* colocar alguma reação quando clica no botão "Adicionar" */
+  return ( 
 
     <div className="content">  
       <p> 
         Escreva os dados do <strong>funcionário</strong> que deseja adicionar
       </p>
 
-      <form onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <label htmlFor="nome">Nome:</label>
         <input 
           type="text" 
           id="nome"
+          maxLength="30"
+          minLength="2"
           placeholder="2 a 30 letras"
           onChange={event => funcionario.nome = event.target.value}
+          title="de 2 a 30 letras"
+          pattern="{2,30}"
+          required
           />
         <label htmlFor="sobrenome">Sobrenome:</label>
         <input 
           type="text" 
-          id="sobrenome" 
+          id="sobrenome"
+          maxLength="50"
+          minLength="2"
           placeholder="de 2 a 50 letras"
           onChange={ event => funcionario.sobrenome = event.target.value}
+          title="de 2 a 50 letras"
+          pattern="{2,50}"
+          required
           />
           <label htmlFor="email">E-Mail *</label>
         <input 
           type="email" 
           id="email" 
-          placeholder="E-mail do funcionário"
+          placeholder="Digite um e-mail válido do funcionário"
           onChange={event => funcionario.email = event.target.value}
+          title="Digite um e-mail váli do funcionário"
+          required
           />
-          <label htmlFor="nome">Nis:</label>
+          <label htmlFor="nome">nis:</label>
         <input 
           type="text" 
-          id="nis" 
-          placeholder="digite o nis (apenas números)"
+          id="nis"
+          maxLength="11"
+          minLength="11"
+          placeholder="digite o nis (somente números)"
           onChange={event => funcionario.nis = event.target.value}
+          title="somente números"
+          pattern="[0-9]*"
+          required
           />
         
-        <button className="btn">Adicionar</button>
+        <button className="btn" onClick={() => handleSubmit()}>Adicionar</button>
         
       </form>
-      <Link to="/">
+
         <button className="btn_home">Home</button>
-      </Link>
         
     </div>  
   )
